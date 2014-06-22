1bbf8e07c538186ce3a45eef2ba0fa47f186a6e7
Rynoxx <rynoxx.spammail@gmail.com>
2014-06-22 17:04:52 +0200

    Update cl_chatting.lua
    Fixed an error that occured, where it for some reason couldn't get plySpeaker:Nick()

--- gamemode/core/clientfiles/cl_chatting.lua
+++ gamemode/core/clientfiles/cl_chatting.lua
@@ -13,6 +13,8 @@ function GM:OnPlayerChat(plySpeaker, strText, boolTeamOnly, boolPlayerIsDead)
 	if tblText[1] == "*" and tblText[#tblText] == "*" && #tblText > 2 then clrChat = clrGreen end
 	if tblText[1] == "/" && tblText[2] == "/" && #tblText > 2 then clrChat = clrOrange end
 	if tblText[#tblText] == "@" && #tblText > 1 then clrChat = clrCream end
+	local speakerNick = "Console"
+	if IsValid(plySpeaker) and plySpeaker.Nick then speakerNick = plySpeaker:Nick() end
 	if boolDisplayName then
 		if clrChat == clrOrange then
 			if tblText[3] == " " then
@@ -21,9 +23,10 @@ function GM:OnPlayerChat(plySpeaker, strText, boolTeamOnly, boolPlayerIsDead)
 				strText = string.sub( strText, 2, #tblText )
 			end
 			strText = string.Replace( strText, "/", "" )
-			chat.AddText( clrChat, "[OOC] ", clr_Gray, clrPlayerName, plySpeaker:Nick(), ": ",  clrWhite,  strText)
+			
+			chat.AddText( clrChat, "[OOC] ", clr_Gray, clrPlayerName, speakerNick, ": ",  clrWhite,  strText)
 		else
-			chat.AddText(clrPlayerName, plySpeaker:Nick(), ": ", clrChat,  strText)
+			chat.AddText(clrPlayerName, speakerNick, ": ", clrChat,  strText)
 		end
 	else
 		chat.AddText(clrChat, strText)
